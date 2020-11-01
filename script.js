c1d5d0cfba7ba5fbcfde763e7828afcc690a3dad
efernandes16 <ericafernandesrj@yahoo.com.br>
2020-11-01 17:55:45 -0300

    Adicionando Script e HTML alterado

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-function calcular (valor1, valor1, operacao) {
+function calcular () {
 
     var valor1 = document.getElementById ('numero1').value  // tem que guardar o valor do 1º input
     var valor2 = document.getElementById('numero2').value // tem que guardar o valor do 2º input
     var operacao = document.getElementById('operacao').value; //monta a operação
     var resultado = document.querySelector('.texto') // tem que mostrar o resultado da operação
-  
 
     switch (operacao)  {
     case "somar":
-        var calculo = parseInt(valor1) + parseInt (valor2)
-    return resultado.innerHTML=  `O resultado da operação: ${calculo}`
+        var calculo = parseFloat(valor1) + parseFloat(valor2)
+    return resultado.innerHTML=  `O resultado da operação: ${calculo.toFixed(2)}`
        break;
     case "subtrair":
-        calculo = parseInt(valor1) - parseInt (valor2)
-        return resultado.innerHTML = `O resultado da operação é: ${calculo}`
+        calculo = parseFloat(valor1) - parseFloat(valor2)
+        return resultado.innerHTML = `O resultado da operação é: ${calculo.toFixed(2)}`
     case "multiplicar":
-        calculo = parseInt(valor1) * parseInt (valor2)
-        return resultado.innerHTML = `O resultado da operaçãoé: ${calculo}`
+        calculo = parseFloat(valor1) * parseFloat(valor2)
+        return resultado.innerHTML = `O resultado da operaçãoé: ${calculo.toFixed(2)}`
     case "dividir":
-        calculo = parseInt(valor1) - parseInt (valor2)
-        return resultado.innerHTML = `O resultado da operação é: ${calculo}`
+        calculo = parseFloat(valor1) / parseFloat (valor2)
+        return resultado.innerHTML = `O resultado da operação é: ${calculo.toFixed(2)}`
     default:
         break
 
